14c382369e3c70fe8b1b43decc7b6208c3ee62a7
yuruijia <ruijia.yu.20@ucl.ac.uk>
2025-03-19 23:12:05 +0000

    UI面板：史莱姆颜色选择

--- Assets/avatar-example/ChangeColor.cs
+++ Assets/avatar-example/ChangeColor.cs
@@ -3,95 +3,40 @@ using Ubiq.Avatars;
 using Ubiq.Messaging;
 using Ubiq.Rooms;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using Random = UnityEngine.Random;
 
-/// <summary>
-/// This class listens to the select event of an XRI interactable, then sets the
-/// avatar to a simple example avatar and gives it a random color. It does not
-/// need to send any messages or listen to peer events etc because it uses the
-/// AvatarManager to swap prefab and the SimpleColorAvatar class to swap color.
-///
-/// SimpleColorAvatar has extensive comments, go have a look!
-/// </summary>
 public class ChangeColor : MonoBehaviour
 {
-    public GameObject prefab;
-
-    private XRSimpleInteractable interactable;
+    public GameObject prefab; // Avatar Prefab (optional)
+    public Material[] colorMaterials; // Store 4 color materials
+    
     private RoomClient roomClient;
     private AvatarManager avatarManager;
-    
+
     private void Start()
     {
-        // Connect up the event for the XRI button.
-        interactable = GetComponent<XRSimpleInteractable>();
-        interactable.selectEntered.AddListener(Interactable_SelectEntered);
-        
-        var networkScene = NetworkScene.Find(this); 
+        // Find the Network Avatar Manager
+        var networkScene = NetworkScene.Find(this);
         roomClient = networkScene.GetComponentInChildren<RoomClient>();
         avatarManager = networkScene.GetComponentInChildren<AvatarManager>();
     }
 
-    private void OnDestroy()
+    // Function to change avatar color via UI button
+    public void SetColor(int colorIndex)
     {
-        // Cleanup the event for the XRI button so it does not get called after
-        // we have been destroyed.
-        if (interactable)
-        {
-            interactable.selectEntered.RemoveListener(Interactable_SelectEntered);
-        }
-    }
+        if (!avatarManager || colorMaterials.Length == 0) return;
 
-    private void Interactable_SelectEntered(SelectEnterEventArgs arg0)
-    {
-        // The button has been pressed.
-        
-        // Change the local avatar prefab to the simple example prefab. The
-        // AvatarManager will do the work of letting other peers know about the
-        // prefab change.
-        avatarManager.avatarPrefab = prefab;
-        
-        // Also, set the color to a new, random one. We use a coroutine to
-        // wait a few frames to allow the AvatarManager time to spawn the new
-        // prefab.
-        StartCoroutine(SetRandomColor());
-    }
-    
-    // This is a coroutine. They can be used in Unity to spread work out over 
-    // multiple frames. They can be paused with a 'yield' instruction. When
-    // the yield ends, they will pick up again wherever they left off.
-    private IEnumerator SetRandomColor()
-    {
-        while (true)
+        // Find the current avatar in the scene
+        var avatar = avatarManager.FindAvatar(roomClient.Me);
+        if (avatar)
         {
-            if (!avatarManager)
+            Renderer avatarRenderer = avatar.GetComponentInChildren<Renderer>();
+            if (avatarRenderer && colorIndex >= 0 && colorIndex < colorMaterials.Length)
             {
-                // Yield break ends the coroutine.
-                yield break;
+                avatarRenderer.material = colorMaterials[colorIndex]; // Set new material
+                Debug.Log("Changed Avatar color to: " + colorMaterials[colorIndex].name);
             }
-            
-            var avatar = avatarManager.FindAvatar(roomClient.Me);
-            if (avatar)
-            {    
-                var colorAvatar = avatar.GetComponentInChildren<SimpleColorAvatar>();
-                if (colorAvatar)
-                {
-                    // Set the random color.
-                    var randomColor = Random.ColorHSV();
-                    colorAvatar.SetColor(randomColor);
-                    
-                    // End the coroutine.
-                    yield break;
-                }
-            }
-            
-            // Yield return null pauses the coroutine until the next frame. We
-            // wait a few frames to allow the prefab to be spawned and to
-            // initialise itself.
-            yield return null;
-            yield return null;
         }
     }
 }
